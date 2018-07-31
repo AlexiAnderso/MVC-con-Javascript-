f60696cbcd19a1ddba287daf5cb2f8e9f0a74c60
Alexis Quesada Chumbes <pirex157@hotmail.com>
2018-07-31 18:00:42 -0500

    Add files via upload

---
+++ Demos/Demo09/Controllers/ProductoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Northwind.Librerias.EntidadesNegocio;
+using Northwind.Librerias.ReglasNegocio;
+using General.Librerias.CodigoUsuario;
+
+namespace Demo07.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+
+		public string listar() {
+			string rpta = "";
+			brProducto obrProducto = new brProducto();
+			List<beProducto> lbeProducto = obrProducto.Listar();
+			if (lbeProducto != null && lbeProducto.Count > 0)
+			{
+				rpta = Objeto.Serializar(lbeProducto, '|',';',false,"",false);
+			}
+
+			return rpta;
+		}
+    }
+}
